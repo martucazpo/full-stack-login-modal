61dd24629277be428e6661ba731be94a88c0241f
martucazpo <martucazpo@gmail.com>
2020-08-19 07:44:36 -0700

    reset btn

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,20 +1,21 @@
-console.log("I'm in app.js");
-// document.addEventListener('DOMContentLoaded', () => {
-//     const nameDiv = document.getElementById("name");
-//     const ageDiv = document.getElementById("age");
-//     const idDiv = document.getElementById("id");
-//     const fciDiv = document.getElementById("fci");
-//     const name = nameDiv.getAttribute('data-value');
-//     const age = ageDiv.getAttribute('data-value');
-//     const id = idDiv.getAttribute('data-value');
-//     const fci = fciDiv.getAttribute('data-value');
-//     console.log(name, age, id, fci);
-// });
-const nameDiv = document.getElementById("name");
-const name = nameDiv.getAttribute('data-value');
-console.log(name);
+const idDiv = document.getElementById("id");
+const id = idDiv.getAttribute("data-value");
+const playerIdDiv = document.getElementById("player_id");
+const playerId = playerIdDiv.getAttribute('data-value');
+const scoreDBDiv = document.getElementById("score");
+const resetBtn = document.getElementById("resetBtn");
+const timesPlayedDiv = document.getElementById("times_played");
+const timesPlayed = timesPlayedDiv.getAttribute('data-value');
+console.log("timesPlayed: " + timesPlayed);
 const ageDiv = document.getElementById("age");
 const age = ageDiv.getAttribute('data-value');
+const winsTextP = document.getElementById("winsText");
+const lossesTextP = document.getElementById("lossesText");
+const target1 = document.getElementById("target1");
+const target2 = document.getElementById("target2");
+const target3 = document.getElementById("target3");
+const playerNumber = document.getElementById("playerNumber");
+const targetNumber = document.getElementById("targetNumber");
 const randomNum = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
 const makeRange = (age) => {
     if (age < 10) {
@@ -26,8 +27,8 @@ const makeRange = (age) => {
     }
 };
 
+let score = scoreDBDiv.getAttribute('data-value');
 let randomNumber = 0;
-let score = 0;
 let wins = 0;
 let losses = 0;
 let val1 = 0;
@@ -35,22 +36,26 @@ let val2 = 0;
 let val3 = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
-    setUpDivs();
     setUpGame();
     gameNumbers();
+});
+
+resetBtn.addEventListener('click', () => {
+    score = 0;
+    updateScore(score);
 });
 
 function setUpGame() {
     targetNum = 0;
     playerNum = 0;
-    randomNumber = randomNum(20,1);
+    randomNumber = randomNum(20, 1);
     targetNum = age * randomNumber;
     val1 = makeRange(age);
     val2 = makeRange(age);
     val3 = makeRange(age);
-    scoreDiv.innerText = "Score: " + score;
-    winsDiv.innerHTML = "<p>Wins: " + wins + "</p><div class='wins-losses-img-div wins-img-div'></div>";
-    lossesDiv.innerHTML = "<p>Losses: " + losses + "</p><div class='wins-losses-img-div losses-img-div'></div>";
+    winsTextP.innerText = "Wins: " + wins;
+    lossesTextP.innerText = "Losses: " + losses;
+    scoreDBDiv.innerText = "Score: " + score;
     playerNumber.innerText = "Player Number: " + playerNum;
     targetNumber.innerText = "Game Number: " + targetNum;
     target1.innerText = val1;
@@ -77,7 +82,7 @@ function play(val) {
 }
 
 function match(playerNum, targetNum) {
-    if (playerNum > targetNum) {
+    if (playerNum >= targetNum + 1) {
         lose();
     }
     if (playerNum === targetNum) {
@@ -87,90 +92,50 @@ function match(playerNum, targetNum) {
 
 function lose() {
     losses++;
-    lossesDiv.innerText = "Losses: " + losses;
+    lossesTextP.innerText = "Losses: " + losses;
     matchScores();
     setUpGame();
 }
 
 function win() {
     wins++;
-    winsDiv.innerText = "Wins: " + wins;
+    winsTextP.innerText = "Wins: " + wins;
     matchScores();
     setUpGame();
 }
 
 function matchScores() {
-    console.log('matchedScores');
     if (losses === 3) {
         score--;
+        updateScore(score);
         wins = 0;
         losses = 0;
-        scoreDiv.innerText = "Score: " + score;
-        winsDiv.innerText = "Wins: " + wins;
-        lossesDiv.innerText = "Losses: " + losses;
+        winsTextP.innerText = "Wins: " + wins;
+        lossesTextP.innerText = "Losses: " + losses;
     } else if (wins === 3) {
         score++;
+        updateScore(score);
         wins = 0;
         losses = 0;
-        scoreDiv.innerText = "Score: " + score;
-        winsDiv.innerText = "Wins: " + wins;
-        lossesDiv.innerText = "Losses: " + losses;
+        winsTextP.innerText = "Wins: " + wins;
+        lossesTextP.innerText = "Losses: " + losses;
     }
 }
 
-function setUpDivs(){
-    const scoreWrapper = document.createElement("div");
-    scoreWrapper.setAttribute("id", "scoreWrapper");
-    scoreWrapper.setAttribute("class", "score-wrapper");
-    const scoreDiv = document.createElement("div");
-    scoreDiv.setAttribute("id", "scoreDiv");
-    scoreDiv.setAttribute("class", "score-div");
-    scoreWrapper.appendChild(scoreDiv);
-
-    const winsDiv = document.createElement("div");
-    winsDiv.setAttribute("id", "winsDiv");
-    winsDiv.setAttribute("class", "wins-and-losses-div wins-div");
-    scoreWrapper.appendChild(winsDiv);
-
-    const lossesDiv = document.createElement("div");
-    lossesDiv.setAttribute("id", "lossesDiv");
-    lossesDiv.setAttribute("class", "wins-and-losses-div losses-div");
-    scoreWrapper.appendChild(lossesDiv);
-
-    const targetWrapper = document.createElement("div");
-    targetWrapper.setAttribute("id", "targetWrapper");
-    targetWrapper.setAttribute("class", "target-wrapper");
-    const targetNumber = document.createElement("div");
-    targetNumber.setAttribute("id", "targetNumber");
-    targetNumber.setAttribute("class", "target-number");
-    targetWrapper.appendChild(targetNumber);
-    const playerNumber = document.createElement("div");
-    playerNumber.setAttribute("id", "playerNumber");
-    playerNumber.setAttribute("class", "player-number");
-    targetWrapper.appendChild(playerNumber);
-    
-    const numberWrapper = document.createElement("div");
-    numberWrapper.setAttribute("id", "numberWrapper");
-    numberWrapper.setAttribute("class", "number-wrapper");
-
-    const target1 = document.createElement("button");
-    target1.setAttribute("id", "target1");
-    target1.setAttribute("class", "game-btn target1");
-    numberWrapper.appendChild(target1);
-
-    const target2 = document.createElement("button");
-    target2.setAttribute("id", "target2");
-    target2.setAttribute("class", "game-btn target2");
-    numberWrapper.appendChild(target2);
-    
-    const target3 = document.createElement("button");
-    target3.setAttribute("id", "target3");
-    target3.setAttribute("class", "game-btn target3");
-    numberWrapper.appendChild(target3);
-    
-    const gameDiv = document.getElementById("gameDiv");
-    gameDiv.appendChild(scoreWrapper);
-    gameDiv.appendChild(targetWrapper);
-    gameDiv.appendChild(numberWrapper);
-    
+function updateScore(score) {
+    let newScore = score;
+    return axios.post('/restOfSite/game/updateScore/' + playerId, {
+        score: newScore
+    })
+    .then((data) => {
+        if (data.status === 200){
+            return scoreDBDiv.innerText = "Score: " + score;
+        } else {
+            console.log("there has been a problem");
+        }
+    })
+    .catch((err) => console.log(err));
 }
+
+
+
--- routes/site/game.js
+++ routes/site/game.js
@@ -1,9 +1,95 @@
 const router = require('express').Router();
 const CatFancier = require('../../models/CatFancier');
+const GamePlayer = require('../../models/GamePlayer');
 const {
     ensureAuthenticated
 } = require('../../passport/auth');
 
+
+router.get('/game/:id', ensureAuthenticated, (req, res) => {
+    let id = req.params.id;
+    CatFancier.findById({ _id: id }, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let name = data.name;
+            let age = data.age;
+            let id = data._id;
+            let fci = data.favoriteCatImg;
+            let user_id = data.user_id;
+            GamePlayer.findOne({ cat_id : id }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (data) {
+                        let player_id = data._id;
+                        let score = data.score;
+                        let times_played = data.times_played;
+                        res.render('layouts/site/rest-of-site.ejs', { name, id, age, fci, user_id, player_id, score, times_played });
+                    } else if (!data) {
+                        let gamePlayer = new GamePlayer({
+                            cat_id : id,
+                            user_id : user_id
+                        });
+                        gamePlayer.save((err, data) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                let player_id = data._id;
+                                let score = data.score;
+                                let times_played = data.times_played;
+                                res.render('layouts/site/rest-of-site.ejs', { name, id, age, fci, user_id, player_id, score, times_played });
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
+});
+
+
+router.post('/game', ensureAuthenticated, (req, res) => {
+    let id = req.body.id;
+    CatFancier.findById({ _id: id }, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let name = data.name;
+            let age = data.age;
+            let id = data._id;
+            let fci = data.favoriteCatImg;
+            let user_id = data.user_id;
+            GamePlayer.findOne({ cat_id : id }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (data) {
+                        let player_id = data._id;
+                        let score = data.score;
+                        let times_played = data.times_played;
+                        res.render('layouts/site/rest-of-site.ejs', { name, id, age, fci, user_id, player_id, score, times_played });
+                    } else if (!data) {
+                        let gamePlayer = new GamePlayer({
+                            cat_id : id,
+                            user_id : user_id
+                        });
+                        gamePlayer.save((err, data) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                let player_id = data._id;
+                                let score = data.score;
+                                let times_played = data.times_played;
+                                res.render('layouts/site/rest-of-site.ejs', { name, id, age, fci, user_id, player_id, score, times_played });
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
+});
 
 router.get('/howTo/:id', ensureAuthenticated, (req, res) => {
     let id = req.params.id;
@@ -13,12 +99,26 @@ router.get('/howTo/:id', ensureAuthenticated, (req, res) => {
         } else {
             let name = data.name;
             let age = data.age;
-            let id = data.id;
+            let id = data._id;
             let fci = data.favoriteCatImg;
             let user_id = data.user_id;
             res.render('layouts/site/how-to.ejs', { name, id, age, fci, user_id });
         }
     });
 });
+
+router.post('/updateScore/:id', (req, res) => {
+    let playerId = req.params.id;
+    let newScore = req.body.score;
+    GamePlayer.findByIdAndUpdate({ _id : playerId }, { $set:{ score : newScore } }, { new: true }, (err,data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let cat_id = data.cat_id;
+            res.redirect('/restOfSite/game/' + cat_id);
+        }
+    });
+});
+
 
 module.exports = router;
